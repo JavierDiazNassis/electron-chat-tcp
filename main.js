2ca331c72a8fd0e36e846a48005a224a2142751c
JavierDiazNassis <jesusnassis105@hotmail.com>
2019-04-26 12:11:42 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,35 @@
+const electron = require('electron');
+const { app, BrowserWindow } = electron;
+
+const path = require('path');
+const url = require('url');
+let win;
+function createWindow() {
+	win = new BrowserWindow({
+		width: 800,
+		height: 600,
+		closable: false,
+		darkTheme: true,
+		resizable: false
+	});
+	win.loadURL(
+		url.format({
+			pathname: path.join(__dirname, 'index.html'),
+			protocol: 'file',
+			slashes: true
+		})
+	);
+}
+
+app.on('ready', createWindow);
+
+exports.openWindow = () => {
+	let newWin = new BrowserWindow({ width: 800, height: 600, closable: false, darkTheme: true, resizable: false });
+	newWin.loadURL(
+		url.format({
+			pathname: path.join(__dirname, 'chat.html'),
+			protocol: 'file',
+			slashes: true
+		})
+	);
+};
